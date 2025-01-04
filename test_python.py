3402cd4bbf0f10350d45c4be21ea8f62551cda28
ZoricaRadakovic <predrag.radakovic4@gmail.com>
2025-01-04 10:12:32 +0100

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -1,8 +1,8 @@
 someNumber = 10 
-if(someNumber<=0):
-    print("less or below to zerro")
-elif (someNumber<0):
+if(someNumber<0):
     print("less then zerro")
+elif (someNumber=0):
+    print("belong to zerro")
 else:
     print ("over zero")    
     
